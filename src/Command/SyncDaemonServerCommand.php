4b321730ccaa0ece7e4e643827fc6b1f65b60ec2
ivixlabs <ivixlabs@gmail.com>
2014-06-24 18:07:24 +0700

    Fix bug.

--- src/Command/SyncDaemonServerCommand.php
+++ src/Command/SyncDaemonServerCommand.php
@@ -6,7 +6,7 @@ use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 
-class SyncDaemonCommand extends ContainerAwareCommand
+class SyncDaemonServerCommand extends ContainerAwareCommand
 {
     /**
      * @var SyncDaemonServer
